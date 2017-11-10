b1fc07046d133037459a559efba9662b89a73f47
John Long <jplong91@gmail.com>
2017-11-09 20:56:26 -0600

    Add number of topics

--- reddit_app.rb
+++ reddit_app.rb
@@ -7,3 +7,12 @@ print "Please enter a subreddit you would like to view: "
 input_subreddit = gets.chomp
 
 response = Unirest.get("https://www.reddit.com/r/#{input_subreddit}/.json")
+subreddit = response.body
+
+number_of_topics = subreddit["data"]["children"].length - 1
+
+index = 0
+number_of_topics.times do
+  p subreddit["data"]["children"][index]["data"]["title"]
+  index += 1
+end
