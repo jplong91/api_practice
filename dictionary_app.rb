f0f13d2a2baffa3b4361713e7065bcdfe2cc5fad
John Long <jplong91@gmail.com>
2017-11-09 19:34:21 -0600

    Definition added

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -7,4 +7,5 @@ input_word = gets.chomp
 response = Unirest.get("http://api.wordnik.com:80/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5")
 body = response.body
 
-puts body[0]["text"]
+definition = body[0]["text"]
+puts "Definition: #{definition.downcase}"
